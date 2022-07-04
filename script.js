61fac5faabcad004e2d0275d248c621bb88b5635
Parisa Ettehad <92958591+parisa-ettd@users.noreply.github.com>
2022-07-04 14:34:11 -0400

    main

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-
+const menu = document.querySelector(".menuli");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger= document.querySelector("menu");
+const closeIcon= document.querySelector(".fa-xmark");
+const menuIcon = document.querySelector(".-fa-bars");
 const mybutton = document.getElementById("myBtn");
+
+function menuFunction() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
