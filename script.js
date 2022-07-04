16f5e9e4ff2bac4a2f1d62caf6699d792924c439
Parisa Ettehad <92958591+parisa-ettd@users.noreply.github.com>
2022-07-04 13:52:39 -0400

    main

---
+++ script.js
@@ -0,0 +1,6 @@
+
+const mybutton = document.getElementById("myBtn");
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
